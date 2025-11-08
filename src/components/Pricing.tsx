78cf437bb896538438bd4fa092a6ff1faecbf8c2
VRtueux <contact@vrtueux.fr>
2025-11-08 19:20:15 +0100

    vrtueux

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -141,7 +141,7 @@ export function Pricing() {
 
                 {/* Cadeau Noel */}
         <div className="mb-16">
-          <h3 className="text-2xl text-white mb-8 text-center">VR Christmas</h3>
+          <h3 className="text-2xl text-white mb-8 text-center">Cartes Cadeaux : VR Christmas</h3>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {cn.map((item, index) => (
               <div
